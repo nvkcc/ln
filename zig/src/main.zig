519b640daa3c6fa2961283089dccc39b869199a3
nguyenvukhang <brew4k@gmail.com>
2026-04-26 11:35:11 +0800

    Can tell from zig whether or not we're in a tty

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,27 +1,12 @@
 const std = @import("std");
 const ln = @import("ln");
+const l = std.os.linux;
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try ln.bufferedPrint();
-}
-
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    const isatty = std.posix.isatty(std.fs.File.stdout().handle);
+    if (isatty) {
+        std.debug.print("IS A TTY!", .{});
+    } else {
+        std.debug.print("IS NOT TTY!", .{});
+    }
 }
