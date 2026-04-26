a9fc94753cbe41cf5fd4e7be8b9db033bc56bf67
nguyenvukhang <brew4k@gmail.com>
2026-04-26 12:45:01 +0800

    Switch to debug allocator

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -7,16 +7,33 @@ const App = @import("app.zig");
 
 pub fn main() !void {
     const app: App = App.init();
+    var gpa = std.heap.DebugAllocator(.{}){};
 
     std.debug.print("rows: {any}\n", .{app.maxOutputRows()});
+    const git_log_argv: [4][]const u8 = .{ "git", "log", "--graph", "--oneline" };
+    var proc = std.process.Child.init(&git_log_argv, gpa.allocator());
+    proc.stdout_behavior = .Pipe;
+    try proc.spawn();
+
+    // var f_reader = proc.stdout.?.reader(&buffer);
+    // var reader = f_reader.interface;
+    // while (true) {
+    //     const len = try reader.readSliceShort(&chunk);
+    //     if (len == 0) break;
+    //     std.debug.print("Chunk: {s}\n", .{chunk[0..len]});
+    // }
+
+    const term = try proc.wait();
+    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+    // child.spawn();
 
     // var wsz: std.posix.winsize = undefined;
     // const ioctl_res = l.ioctl(l.STDOUT_FILENO, l.T.IOCGWINSZ, @intFromPtr(&wsz));
     // if (ioctl_res == -1) {}
 
     if (app.is_atty) {
-        std.debug.print("IS A TTY!", .{});
+        std.debug.print("IS A TTY!\n", .{});
     } else {
-        std.debug.print("IS NOT TTY!", .{});
+        std.debug.print("IS NOT TTY!\n", .{});
     }
 }
