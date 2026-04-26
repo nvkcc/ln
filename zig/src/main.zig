1287440d71c8f7ff5698cf4664ed0f0014fd7ea3
nguyenvukhang <brew4k@gmail.com>
2026-04-26 12:02:29 +0800

    Parse CLI args on zig

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -2,9 +2,24 @@ const std = @import("std");
 const ln = @import("ln");
 const l = std.os.linux;
 
+const App = struct { is_bounded: bool, is_atty: bool };
+
+fn getBoundedFlagFromCli() bool {
+    for (std.os.argv) |argv| {
+        if (std.mem.eql(u8, std.mem.span(argv), "--bound")) {
+            return true;
+        }
+    }
+    return false;
+}
+
 pub fn main() !void {
-    const isatty = std.posix.isatty(std.fs.File.stdout().handle);
-    if (isatty) {
+    const app: App = .{
+        .is_bounded = getBoundedFlagFromCli(),
+        .is_atty = std.posix.isatty(std.fs.File.stdout().handle),
+    };
+
+    if (app.is_atty) {
         std.debug.print("IS A TTY!", .{});
     } else {
         std.debug.print("IS NOT TTY!", .{});
